2c56c1878af850102f296247a4461759acd5c2ab
devops-arch-cloud <57580340+devops-arch-cloud@users.noreply.github.com>
2023-10-15 20:56:31 -0500

    Update seed_jobdsl.groovy

--- seed_jobdsl.groovy
+++ seed_jobdsl.groovy
@@ -1,17 +1,11 @@
 pipelineJob('aurora-postgres-cluster-existing-vpc') {
    definition {
-                cpsScm {
-                    scm {
-                        git {
-                            remote {
-                                github("devops-arch-cloud/test-devops1")
-                               
-                            }
-    
-                            branch('main')
-                        }
-                    }
-    
-}
+               
+    cps {
+      script(readFileFromWorkspace('logic/productPipeline.jenkinsfile'))
+      script(readFileFromWorkspace('pipeline1'))
+      sandbox()     
+    }
+ 
    }
 }
