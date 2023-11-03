73d4e3979ab1ba1b1269d3d62a5afef5062fe6b4
Mitesh Purohit <57580340+devops-arch-cloud@users.noreply.github.com>
2023-11-03 08:42:41 -0500

    Update seed_jobdsl.groovy

--- pipelines/seed_jobdsl.groovy
+++ pipelines/seed_jobdsl.groovy
@@ -50,10 +50,10 @@ pipelineJob('aurora-postgres-cluster-existing-vpc') {
    definition {
                
     cps {
-      script(readFileFromWorkspace('aurora-monitoring'))
-      script(readFileFromWorkspace('aurora-postgres-cluster-existing-vpc'))
-      script(readFileFromWorkspace('aurora-postgres-cluster-global-db'))
-      script(readFileFromWorkspace('db-proxy-to-existing-postgres-cluster'))
+      script(readFileFromWorkspace('pipelines/aurora-monitoring'))
+      script(readFileFromWorkspace('pipelines/aurora-postgres-cluster-existing-vpc'))
+      script(readFileFromWorkspace('pipelines/aurora-postgres-cluster-global-db'))
+      script(readFileFromWorkspace('pipelines/db-proxy-to-existing-postgres-cluster'))
       sandbox()     
     }
  
